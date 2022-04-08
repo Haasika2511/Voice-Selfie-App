51e61618f186bb885407e49c1d6d76eb1996b2fc
Haasika2511 <83579175+Haasika2511@users.noreply.github.com>
2022-04-08 14:53:15 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -17,19 +17,14 @@ recognition.onresult = function (event) {
     }
 
 }
-function speak() {
-    var synth = window.speechSynthesis;
-    speak_data = "Taking your selfie in 5 seconds";
-    var utterThis = new SpeechSynthesisUtterance(speak_data);
-    synth.speak(utterThis);
-    setTimeout();
-    Webcam.attach(camera);
-    setTimeout(function () {
-        take_snapshot();
-        save();
-    }, 5000);
-    
-}
+function speak(){ var synth = window.speechSynthesis; 
+    speak_data = "Taking you Selfie in 5 seconds";
+     var utterThis = new SpeechSynthesisUtterance(speak_data);
+      synth.speak(utterThis);
+ Webcam.attach(camera);
+ setTimeout(function() { take_snapshot();
+     save(); }, 5000); 
+    }
 
 
 Webcam.set({
