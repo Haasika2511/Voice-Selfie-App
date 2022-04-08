7442122bd2b3103070f0a273be1804de05821f46
Haasika2511 <83579175+Haasika2511@users.noreply.github.com>
2022-04-08 14:48:29 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -26,7 +26,9 @@ function speak() {
     Webcam.attach(camera);
     setTimeout(function () {
         take_snapshot();
+        save();
     }, 5000);
+    
 }
 
 
@@ -42,4 +44,11 @@ function take_snapshot() {
     Webcam.snap(function (data_uri) {
         document.getElementById("result").innerHTML = '<img id="selfie_image" src=' + data_uri + '>';
     });
+}
+
+function save() {
+    link = document.getElementById("link");
+    image = document.getElementById("selfie_image").src;
+    link.href = img;
+    link.click();
 }
